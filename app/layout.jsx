6c1abec9cf36093848ba4c7fccd20cb633cf0a29
naxim-khan <nazeemkhanpk@gmail.com>
2025-01-13 00:12:03 +0500

    name updated

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from "@/components/Nav";
 import Provider from "@/components/Provider";
 
 export const metadata = {
-  title: "Promptopia",
+  title: "PromptSynergy",
   description: "Discover & Share AI Prompts",
 };
 
--- app/page.jsx
+++ app/page.jsx
@@ -5,19 +5,19 @@ import Feed from '@/components/Feed'
 const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
-            <h1 className='head_text text-center'>
-            Discover & Share &nbsp;
-            <br className='max-md:hidden' />
-            <span className='orange_gradient text-center'>
-            AI-Powered Prompts
-            </span>
-            </h1>
+      <h1 className='head_text text-center'>
+        Discover & Share &nbsp;
+        <br className='max-md:hidden' />
+        <span className='orange_gradient text-center'>
+          AI-Powered Prompts
+        </span>
+      </h1>
 
-            <p className='desc text-center'>
-                Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts.
-            </p>
+      <p className='desc text-center'>
+        PromptSynergy is an open-source AI prompting tool for modern world to discover, create and share creative prompts.
+      </p>
 
-            <Feed />
+      <Feed />
     </section>
   )
 }
